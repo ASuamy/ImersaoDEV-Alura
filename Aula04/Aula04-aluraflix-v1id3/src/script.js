d7aca64e5ef6375eff91c8156f56207e1d8b10f3
Asuamy <anandasuamy@gmail.com>
2022-03-16 10:27:27 -0300

    Aula 04

---
+++ Aula04/Aula04-aluraflix-v1id3/src/script.js
@@ -0,0 +1,9 @@
+var seriesFavoritas = [
+  "https://media.fstatic.com/aXlong6yyQGIOOgAdvmGAeKHx9E=/290x478/smart/media/movies/covers/2021/11/FFFFGw3XIAA_kPx.jpg",
+  "https://m.media-amazon.com/images/I/41h+CXDPYkS.jpg",
+  "https://tribunapr.uol.com.br/wp-content/uploads/sites/56/2022/01/20153741/Arquivo-81-poster.jpg"
+];
+
+for (i = 0; i < seriesFavoritas.length; i++) {
+  document.write("<img src=" + seriesFavoritas[i] + ">");
+}
